5375fada946635aa3d6c52347215deb751f60744
PhiMaii <pmairguenther@web.de>
2024-07-17 13:36:03 +0200

    Updated code to display one ball on the screen

--- src/main.py
+++ src/main.py
@@ -1,19 +1,27 @@
+import ball
+
 import pygame
 import sys
 
 pygame.init()
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Particle Simulation')
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill((255, 255, 255))
+    
+    screen.fill(WHITE)
+    
     
     pygame.display.flip()
 
